2253e84e4fcba86ecab5206c8a4e117c1271bbd9
ziihan1 <143859934+ziihan1@users.noreply.github.com>
2023-09-02 10:59:35 +0700

    Add files via upload
    uploadfiledeh

---
+++ test1.py
@@ -0,0 +1,32 @@
+nama_saya = str(input("masukan nama kamu = "))
+print(nama_saya, "bertipe",type(nama_saya))
+
+umur = int(input("masukan umur = "))  
+print (umur, "bertipe", (umur))
+
+IPK = float(input("masukan ipk = "))
+print(IPK, "bertipe", type (IPK))
+
+belajar = bool(input("masukan jawaban = "))
+print("menikah" ,belajar, "bertipe",type(belajar))
+
+matkul_diambil = list(input("masukan_matkul = "))
+print("Matkul : ",matkul_diambil,"bertipe", type (matkul_diambil))
+
+matkul_diambil2 = tuple(input("masukan_matkul = "))
+print("Matkul_lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+matkul_diambil3 = {"Alpro", "Alpro", "pemweb", "Data Science", "Data Science"}
+print("matkul Lain-Lain: ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+nama_universitas = "Universitas Airlangga"
+print(nama_universitas)
+
+
+
+print("1"+ "1")
+print(1 + 1)
+print("siuu" * 3)
+print(17/3)
+print(17//3)
+print(17%3)
